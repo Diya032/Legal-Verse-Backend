4286d80a0a77f515fdfc3fdd6994725d2f89de98
Diya032 <99545441+Diya032@users.noreply.github.com>
2023-10-08 21:08:09 +0530

    Highlighter/ reviewer addition
    data_to_process.txt: directory change
    script1.py: deleted
    index.js: code changed
    legal_NER: files directory changed

--- nodePythonApp/index.js
+++ nodePythonApp/index.js
@@ -1,28 +1,48 @@
-const express = require('express')
-const {spawn} = require('child_process');
-const app = express()
-const port = 3000
+const express = require('express');
+const { spawn } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 3000;
+
 app.get('/', (req, res) => {
+    const inputdataPath = path.join(__dirname, 'legal_NER', 'data_to_process.txt');
+    var inputdata = fs.readFileSync(inputdataPath, 'utf-8');
+    const pythonScriptPath = path.join(__dirname, 'legal_NER', 'Highlighter.py');
 
-var inputdata="hello";
+    const python = spawn('python', [pythonScriptPath, inputdata, 'second_data']);
+    
+    let htmlContent = '';
+    let pythonError = ''; // Variable to capture Python script error messages
 
+    python.on('error', (error) => {
+        console.error(`Error in Python script: ${error.message}`);
+    });
 
- 
- var dataToSend;
- // spawn new child process to call the python script
- const python = spawn('python', ['input_output_demo.py' , inputdata , 'second_data']);
- // collect data from script
- python.stdout.on('data', function (data) {
-  console.log('Pipe data from python script ...');
-  dataToSend = data.toString();
- });
- // in close event we are sure that stream from child process is closed
- python.on('close', (code) => {
- console.log(`child process close all stdio with code ${code}`);
- // send data to browser
- res.send(dataToSend)
- });
- 
-})
-app.listen(port, () => console.log(`Example app listening on port 
-${port}!`))
+    python.stdout.on('data', (data) => {
+        // Capture the data and convert it to a string
+        htmlContent += data.toString();
+    });
+
+    python.stderr.on('data', (data) => {
+        // Capture Python script error messages
+        pythonError += data.toString();
+    });
+
+    python.on('close', (code) => {
+        console.log(`child process close all stdio with code ${code}`);
+
+        // Log the HTML content
+        console.log(htmlContent);
+
+        // Send the captured HTML content to the client
+        res.send(htmlContent);
+
+        // Log Python script error messages (if any)
+        if (pythonError) {
+            console.error(`Python script error: ${pythonError}`);
+        }
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
